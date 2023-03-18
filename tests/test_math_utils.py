e6d8b1a6e933eff9ac5a0782d6c0e188ccbb4152
Hugo Lemoine <hlemoine@spypoint.com>
2023-03-18 16:19:01 +0000

    math_utils: refactor test to unittest

--- tests/test_math_utils.py
+++ tests/test_math_utils.py
@@ -1,75 +1,81 @@
-
-
+import unittest
 import numpy as np
-import pytest
+
+from parameterized import parameterized
 
 from rlgameoflife import math_utils
 
-def test_init():
-    v = math_utils.Vector2D(1, 2)
-    assert v.vector[0] == 1
-    assert v.vector[1] == 2
 
-def test_magnitude():
-    v = math_utils.Vector2D(3, 4)
-    assert v.magnitude() == 5.0
+class TestMathUtils(unittest.TestCase):
+    def test_init(self):
+        v = math_utils.Vector2D(1, 2)
+        self.assertEqual(v.vector[0], 1)
+        self.assertEqual(v.vector[1], 2)
 
-def test_normalize():
-    v = math_utils.Vector2D(3, 4)
-    n = v.normalize()
-    assert n.magnitude() == 1.0
+    def test_magnitude(self):
+        v = math_utils.Vector2D(3, 4)
+        self.assertEqual(v.magnitude(), 5.0)
 
-def test_scale():
-    v = math_utils.Vector2D(1, 1)
-    s = v.scale(5)
-    assert s.magnitude() == 5.0
+    def test_normalize(self):
+        v = math_utils.Vector2D(3, 4)
+        n = v.normalize()
+        self.assertEqual(n.magnitude(), 1.0)
 
-def test_add():
-    v1 = math_utils.Vector2D(1, 2)
-    v2 = math_utils.Vector2D(3, 4)
-    a = v1.add(v2)
-    assert a.vector[0] == 4
-    assert a.vector[1] == 6
+    def test_scale(self):
+        v = math_utils.Vector2D(1, 1)
+        s = v.scale(5)
+        self.assertEqual(s.magnitude(), 5.0)
 
-def test_subtract():
-    v1 = math_utils.Vector2D(3, 4)
-    v2 = math_utils.Vector2D(1, 2)
-    s = v1.subtract(v2)
-    assert s.vector[0] == 2
-    assert s.vector[1] == 2
+    def test_add(self):
+        v1 = math_utils.Vector2D(1, 2)
+        v2 = math_utils.Vector2D(3, 4)
+        a = v1.add(v2)
+        self.assertEqual(a.vector[0], 4)
+        self.assertEqual(a.vector[1], 6)
 
-def test_dot():
-    v1 = math_utils.Vector2D(1, 2)
-    v2 = math_utils.Vector2D(3, 4)
-    d = v1.dot(v2)
-    assert d == 11.0
+    def test_subtract(self):
+        v1 = math_utils.Vector2D(3, 4)
+        v2 = math_utils.Vector2D(1, 2)
+        s = v1.subtract(v2)
+        self.assertEqual(s.vector[0], 2)
+        self.assertEqual(s.vector[1], 2)
 
-def test_cross():
-    v1 = math_utils.Vector2D(1, 2)
-    v2 = math_utils.Vector2D(3, 4)
-    c = v1.cross(v2)
-    assert c == -2.0
+    def test_dot(self):
+        v1 = math_utils.Vector2D(1, 2)
+        v2 = math_utils.Vector2D(3, 4)
+        d = v1.dot(v2)
+        self.assertEqual(d, 11.0)
 
-@pytest.mark.parametrize("vx1, vy1, vx2, vy2, expected", [
-    (1, 0, 0, 1, np.pi/2),
-    (1, 0, 1, 0, 0),
-    (1, 1, -1, 1, np.pi/2),
-    (1, 0, 0, -1, -np.pi/2),
-    (1, 0, -1, 0, np.pi),
-])
-def test_angle_between(vx1, vy1, vx2, vy2, expected):
-    v1 = math_utils.Vector2D(vx1, vy1)
-    v2 = math_utils.Vector2D(vx2, vy2)
-    angle = v1.angle_between(v2)
-    assert np.isclose(angle, expected, atol=1e-6)
+    def test_cross(self):
+        v1 = math_utils.Vector2D(1, 2)
+        v2 = math_utils.Vector2D(3, 4)
+        c = v1.cross(v2)
+        self.assertEqual(c, -2.0)
 
-@pytest.mark.parametrize("vx, vy, angle, ex, ey", [
-    (1, 0, np.pi/2, 0, 1),
-    (1, 1, np.pi/4, 0, np.sqrt(2)),
-    (1, 0, -np.pi/2, 0, -1),
-])
-def test_rotate(vx, vy, angle, ex, ey):
-    v = math_utils.Vector2D(vx, vy)
-    r = v.rotate(angle)
-    assert np.isclose(r.vector[0], ex, atol=1e-6)
-    assert np.isclose(r.vector[1], ey, atol=1e-6)
+    @parameterized.expand(
+        [
+            [1, 0, 0, 1, np.pi / 2],
+            [1, 0, 1, 0, 0],
+            [1, 1, -1, 1, np.pi / 2],
+            [1, 0, 0, -1, -np.pi / 2],
+            [1, 0, -1, 0, np.pi],
+        ]
+    )
+    def test_angle_between(self, vx1, vy1, vx2, vy2, expected):
+        v1 = math_utils.Vector2D(vx1, vy1)
+        v2 = math_utils.Vector2D(vx2, vy2)
+        angle = v1.angle_between(v2)
+        self.assertAlmostEqual(angle, expected, places=6)
+
+    @parameterized.expand(
+        [
+            (1, 0, np.pi / 2, 0, 1),
+            (1, 1, np.pi / 4, 0, np.sqrt(2)),
+            (1, 0, -np.pi / 2, 0, -1),
+        ]
+    )
+    def test_rotate(self, vx, vy, angle, ex, ey):
+        v = math_utils.Vector2D(vx, vy)
+        r = v.rotate(angle)
+        self.assertAlmostEqual(r.vector[0], ex, places=6)
+        self.assertAlmostEqual(r.vector[1], ey, places=6)
